15c57b3cb30d05930a99e155439580c3c67fd328
KaVik <kaue.o.victorio@outlook.com>
2022-12-06 21:49:19 -0300

    First
    Primeiro Dia de desenvolvimento do projeto final.

---
+++ ProjetoFinalV2/ProjetoFinalV2/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjetoFinalV2;
+
+public partial class Produto
+{
+    public int IdProduto { get; set; }
+
+    public string NomeProduto { get; set; } = null!;
+
+    public int? Quantidade { get; set; }
+
+    public double PrecoCompra { get; set; }
+
+    public double Tributos { get; set; }
+
+    public double PrecoVenda { get; set; }
+
+    public virtual ICollection<Venda> Venda { get; } = new List<Venda>();
+}
